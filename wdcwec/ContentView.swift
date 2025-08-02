a3a72cd46f6935c804df9cd47edb5f83681291ea
ArikB08 <arikbagchi@outlook.com>
2025-08-02 14:36:42 +0800

    Initial Commit

---
+++ wdcwec/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wdcwec
+//
+//  Created by Arik Bagchi on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wdcwec/wdcwecApp.swift
@@ -0,0 +1,17 @@
+//
+//  wdcwecApp.swift
+//  wdcwec
+//
+//  Created by Arik Bagchi on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct wdcwecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
